66b4c73bc1a6ffd0c73066cb823ef80cc19128d0
1st-award <as4290156@gmail.com>
2022-04-04 21:32:28 +0900

    초기 작업

---
+++ draw_Shape.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+
+function draw() {
+    beginShape(TRIANGLES);
+    vertex(30, 75);
+    vertex(40, 20);
+    vertex(50, 75);
+    endShape();
+}
